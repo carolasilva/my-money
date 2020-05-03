01fe6f6555dbc7b75af1b437036e011ca02b13f0
Carolina Alves <carol.alves99@live.com>
2020-05-03 16:11:25 -0300

    Criado usePost

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import useGet from "./useGet"
+import usePost from './usePost'
 
 const url = 'https://mymoney-25404.firebaseio.com/movimentacoes/2020-05.json'
 
 function App() {
   const data = useGet(url)
-  const data2 = useGet('http://httpbin.org/ip')
+  const [postData, post] = usePost(url);
+
+  const saveNew = () => {
+    post({ valor: 10, descricao: 'olá'})
+  }
+
   return (
     <div>
       <h1>My Money</h1>
@@ -14,7 +20,8 @@ function App() {
           !data.loading ? (
             <div>
               {JSON.stringify(data)}
-              {JSON.stringify(data2)}
+              <button onClick={saveNew}>Salvar</button>
+              <pre>{JSON.stringify(postData)}</pre>
             </div>
           ) : (
             <p>Loading...</p>
