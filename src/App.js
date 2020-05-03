68aef3ab9c625ae0e266040c60832a3fa57cfcfd
Carolina Alves <carol.alves99@live.com>
2020-05-03 15:37:29 -0300

    Criando hook personalizado useGet

--- src/App.js
+++ src/App.js
@@ -1,50 +1,28 @@
-import React, { useEffect, useReducer } from 'react'
-import axios from 'axios'
+import React from 'react'
+import useGet from "./useGet"
 
 const url = 'https://mymoney-25404.firebaseio.com/movimentacoes/2020-05.json'
 
-// pure function
-const reducer = (state, action) => {
-  if (action.type === 'REQUEST') {
-    return {
-      ...state,
-      loading: true
-    }
-  }
-  if (action.type === 'SUCCESS') {
-    return {
-      ...state,
-      loading: false,
-      data: action.data
-    }
-  }
-
-  return state
-}
-
 function App() {
-  const [data, dispatch] = useReducer(reducer, {
-    loading: true,
-    data: {}
-  })
-
-  useEffect(() => {
-    dispatch({ type: 'REQUEST' })
-    axios
-      .get(url)
-      .then(res => {
-        dispatch({ type: 'SUCCESS', data: res.data })
-      })
-  }, []);
-
+  const data = useGet(url)
+  const data2 = useGet('http://httpbin.org/ip')
   return (
     <div>
-      <h1>MyMoney</h1>
-
-      { !data.loading && JSON.stringify(data) }
-      { data.loading && <p>Loading... </p>}
+      <h1>My Money</h1>
+      <div>
+        {
+          !data.loading ? (
+            <div>
+              {JSON.stringify(data)}
+              {JSON.stringify(data2)}
+            </div>
+          ) : (
+            <p>Loading...</p>
+          )
+        }
+      </div>
     </div>
-  );
+  )
 }
 
 export default App
