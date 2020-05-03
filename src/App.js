fac41976f4ecb1fdeb0e8811720c329bdd75fbcf
Carolina Alves <carol.alves99@live.com>
2020-05-03 16:20:29 -0300

    Criado useDelete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import useGet from "./useGet"
 import usePost from './usePost'
+import useDelete from './useDelete'
 
 const url = 'https://mymoney-25404.firebaseio.com/movimentacoes/2020-05.json'
 
 function App() {
   const data = useGet(url)
   const [postData, post] = usePost(url);
+  const [deleteData, remove] = useDelete();
 
   const saveNew = () => {
     post({ valor: 10, descricao: 'olá'})
+  }
+
+  const doRemove = () => {
+    remove('https://mymoney-25404.firebaseio.com/movimentacoes/2020-05/a.json')
   }
 
   return (
@@ -22,6 +28,9 @@ function App() {
               {JSON.stringify(data)}
               <button onClick={saveNew}>Salvar</button>
               <pre>{JSON.stringify(postData)}</pre>
+              <button onClick={doRemove}>Delete</button>
+              <pre>{JSON.stringify(deleteData)}</pre>
+
             </div>
           ) : (
             <p>Loading...</p>
