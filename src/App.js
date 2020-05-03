049910bfbf17d2277e78cc4046ac02b9619bab69
Carolina Alves <carol.alves99@live.com>
2020-05-03 15:23:01 -0300

    Projeto iniciado + useReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useReducer } from 'react'
+import axios from 'axios'
+
+const url = 'https://mymoney-25404.firebaseio.com/movimentacoes/2020-05.json'
+
+// pure function
+const reducer = (state, action) => {
+  if (action.type === 'REQUEST') {
+    return {
+      ...state,
+      loading: true
+    }
+  }
+  if (action.type === 'SUCCESS') {
+    return {
+      ...state,
+      loading: false,
+      data: action.data
+    }
+  }
+
+  return state
+}
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, {
+    loading: true,
+    data: {}
+  })
+
+  useEffect(() => {
+    dispatch({ type: 'REQUEST' })
+    axios
+      .get(url)
+      .then(res => {
+        dispatch({ type: 'SUCCESS', data: res.data })
+      })
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>MyMoney</h1>
+
+      { !data.loading && JSON.stringify(data) }
+      { data.loading && <p>Loading... </p>}
     </div>
   );
 }
 
-export default App;
+export default App
