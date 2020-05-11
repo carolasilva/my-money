efe04338e1a6379ca97865e5543dcbc7e5444593
Carolina Alves <carol.alves99@live.com>
2020-05-11 10:52:30 -0300

    Adicionado firebase functions

--- src/pages/Home/AddMonth.js
+++ src/pages/Home/AddMonth.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { Redirect } from 'react-router-dom'
+
+const minAno = 2019
+const maxAno = 2022
 
 const AddMonth = () => {
+  const refAno = useRef()
+  const refMes = useRef()
+  const [redirect, setRedirect] = useState('');
+  const anos = [];
+  const meses = []
+  for (let i = minAno; i <= maxAno; i++) {
+    anos.push(i);
+  }
+  for (let i=1; i<=12; i++) {
+    meses.push(i);
+  }
+
+  const zeroPad = num => {
+    if (num < 10)
+      return '0' + num
+    return num
+  }
+
+  const verMes = () => {
+    setRedirect(refAno.current.value + '-' + refMes.current.value);
+    console.log('ver mes', refAno.current.value, refMes.current.value);
+  }
+
+  if (redirect !== '')
+    return <Redirect to={'/movimentacoes/' + redirect} />
+
   return (
     <>
       <h2>Adicionar mês</h2>
-      <select name="ano" id="ano">
-        <option value="2019">2019</option>
-        <option value="2020">2020</option>
-        <option value="2021">2021</option>
-        <option value="2022">2022</option>
+      <select name="ano" id="ano" ref={refAno}>
+        { anos.map(ano => <option key={ano} value={ano}>{ano}</option>)}
       </select>
-      <select name="mes" id="mes">
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
+      <select name="mes" id="mes" ref={refMes}>
+        { meses.map(zeroPad).map(mes => <option key={mes} value={mes}>{mes}</option>)}
       </select>
-      <button>Adicionar mês</button>
+      <button onClick={verMes}>Adicionar mês</button>
     </>
   )
 }
--- src/pages/Movimentacoes.js
+++ src/pages/Movimentacoes.js
@@ -8,24 +8,26 @@ const Movimentacoes = ({ match }) => {
   const [postData, salvar] = usePost(`movimentacoes/${match.params.data}`)
   const [removeData, remover] = useDelete(`movimentacoes/${match.params.data}`)
   const [descricao, setDescricao] = useState('')
-  const [valor, setValor] = useState(0)
+  const [valor, setValor] = useState('')
 
   const onChangeDescricao = evt => {
     setDescricao(evt.target.value)
   }
 
   const onChangeValor = evt => {
-    setValor(parseFloat(evt.target.value))
+    setValor(evt.target.value)
   }
 
   const salvarMovimentacao = async() => {
-    await salvar({
-      descricao,
-      valor
-    })
-    setDescricao('')
-    setValor(0)
-    data.refetch()
+    if(!isNaN(valor) && valor.search(/^[-]?\d+((\.)?\d+?)?$/) >= 0) {
+      await salvar({
+        descricao,
+        valor: parseFloat(valor)
+      })
+      setDescricao('')
+      setValor('')
+      data.refetch()
+    }
   }
 
   const removerMovimentacao = async(id) => {
@@ -50,11 +52,11 @@ const Movimentacoes = ({ match }) => {
                 .keys(data.data)
                 .map(movimentacao => {
                   return (
-                    <tr>
+                    <tr key={movimentacao}>
                       <td>{data.data[movimentacao].descricao}</td>
-                      <td>
+                      <td className='text-right'>
                         {data.data[movimentacao].valor}
-                        <button onClick={() => removerMovimentacao(movimentacao)}>-</button>
+                        <button className='btn btn-danger ml-3' onClick={() => removerMovimentacao(movimentacao)}>-</button>
                       </td>
                     </tr>
                   )
@@ -66,7 +68,7 @@ const Movimentacoes = ({ match }) => {
             </td>
             <td>
               <input type="text" value={valor} onChange={onChangeValor}/>
-              <button onClick={salvarMovimentacao}>+</button>
+              <button className='btn btn-success ml-2' onClick={salvarMovimentacao}>+</button>
             </td>
           </tr>
         </tbody>
